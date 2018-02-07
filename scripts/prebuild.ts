cfb5dca31d75d1dad91e328b27badf20fbe92b9f
chooblarin <choo.bla.rin@gmail.com>
2018-02-07 12:47:07 +0900

    chore: Setup retrieving api key from env variables

---
+++ scripts/prebuild.ts
@@ -0,0 +1,31 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as ejs from 'ejs';
+
+// Make sure that api key was set in env vars
+
+if (!process.env.MARVEL_API_KEY) {
+  throw new Error('You have to set MARVEL_API_KEY as an environment variable');
+}
+
+const envFilesDir = path.join(__dirname, '../src/environments');
+
+const templateNames = [
+  'environment.ts.template',
+  'environment.prod.ts.template'
+];
+
+const templateFiles = templateNames.map(filename => fs
+  .readFileSync(path.join(envFilesDir, filename), { encoding: 'utf-8' })
+);
+
+const targetNames = templateNames.map(target => target.match(/^(.+)\.template$/)[1]);
+
+templateFiles.forEach((content, i) => {
+  fs.writeFileSync(
+    path.join(envFilesDir, targetNames[i]),
+    ejs.render(content, { ...process.env })
+  );
+});
+
+process.exit(0);
