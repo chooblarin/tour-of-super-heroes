4002d8b857a5ad8b3c4463ff8b1358be4d904395
chooblarin <choo.bla.rin@gmail.com>
2018-02-08 23:27:41 +0900

    feat: Add type to API response

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-
-import { environment } from '../environments/environment'
 import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+import { environment } from '../environments/environment';
+import { Hero } from './hero';
+
+interface Data<T> {
+  results: T[]
+}
+
+interface Response<T> {
+  data: Data<T>
+}
 
 @Injectable()
 export class HeroService {
@@ -12,14 +22,18 @@ export class HeroService {
 
   constructor(private http: HttpClient) { }
 
-  getHeroes(): Observable<any> {
+  getHeroes(): Observable<Hero[]> {
     const path = '/v1/public/characters';
     const url = `${this.marvelApiUrl}${path}?limit=10&apikey=${this.apiKey}`;
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const params = new HttpParams()
       .set('limit', '10')
+      .set('orderBy', '-modified')
       .set('apikey', this.apiKey);
     return this.http
-      .get(`${this.marvelApiUrl}${path}`, { headers, params });
+      .get<Response<Hero>>(`${this.marvelApiUrl}${path}`, { headers, params })
+      .pipe(
+      map(response => response.data.results)
+      )
   }
 }
